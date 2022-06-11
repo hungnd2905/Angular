6a01467ee2e4f1d3a4e5c136a3b76222726f4ec5
hungnd2905 <hungnd2905@gmail.com>
2022-06-11 19:07:23 +0200

    String Interpolation. Property binding. Event binding

--- FirstProject/src/app/servers/servers.component.ts
+++ FirstProject/src/app/servers/servers.component.ts
@@ -5,4 +5,19 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created';
+  }
+}
