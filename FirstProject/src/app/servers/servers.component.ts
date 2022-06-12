b15687970fe41f44f90cd9b14a058b5cea6ca22a
hungnd2905 <hungnd2905@gmail.com>
2022-06-12 17:19:58 +0200

    ngClass CSS class to HTML component conditionally

--- FirstProject/src/app/servers/servers.component.ts
+++ FirstProject/src/app/servers/servers.component.ts
@@ -3,14 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-servers',
   templateUrl: './servers.component.html',
-  // styleUrls: ['./servers.component.css'],
-  styles: [
-    `
-      .online {
-        color: white;
-      }
-    `,
-  ],
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent {
   allowNewServer = false;
