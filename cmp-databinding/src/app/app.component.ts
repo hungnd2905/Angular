db43273ab87258dfc972a3db8cd4bfbd810aaeab
hungnd2905 <hungnd2905@gmail.com>
2022-06-14 16:19:28 +0200

    passing data from parent component to child component

--- cmp-databinding/src/app/app.component.ts
+++ cmp-databinding/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @Component({
   selector: 'app-root',
@@ -6,23 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements: { type: string; name: string; content: string }[] = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
+  serverElements = [
+    {
       type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent,
-    });
-  }
+      name: 'Testing Server',
+      content: 'Just a test',
+    },
+    {
+      type: 'server',
+      name: 'Testing Server',
+      content: 'Just a test',
+    },
+  ];
 }
